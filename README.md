95fb5f95b7f1f17082b588ab15527c01a1ffe388
waherof29502 <39645652+waherof29502@users.noreply.github.com>
2022-09-30 11:34:51 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# jin_react-hook-form
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-rmf79b)
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './style.css';
+import { Form } from './components';
+
+export default function App() {
+  return (
+    <div>
+      <Form />
+    </div>
+  );
+}
